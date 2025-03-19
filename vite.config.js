26d343bd3d822f0b6eacb50215887c6f1650854b
mouhcine <mouhcine.t@hiitconsulting.com>
2025-03-19 01:05:47 +0000

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/appelsoffres/", // Add this line to fix GitHub Pages issue
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://163.172.74.154:8000',
+      "/api": {
+        target: "http://163.172.74.154:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
+});
 
